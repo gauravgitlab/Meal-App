2c9e1a501e557d48aaab07e6c74af1c66a863b80
LAPTOP-LOJHGVFB\gaurav <gauravthailand@gmail.com>
2020-11-01 15:20:15 +1300

    Meal Items added

--- Screens/CategoryMealsScreen.js
+++ Screens/CategoryMealsScreen.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet, Platform} from 'react-native';
+import {View, Text, Button, StyleSheet, Platform, FlatList} from 'react-native';
 import { HeaderTitle } from 'react-navigation-stack';
-import {CATEGORIES} from '../data/dummy-data';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props => {
 
-    const catId = props.navigation.getParam('categoryId')
-    const selectedId = CATEGORIES.find(cat => cat.id === catId);
+    const catId = props.navigation.getParam('categoryId');
+    const dispayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+    
+    const renderMealItem = mealItem => {
+        return (<MealItem 
+            title={mealItem.item.title}
+            onSelectMeal={() => props.navigation.navigate({ routeName: 'MealDetails', params:{mealId : mealItem.item.id}})}
+            imageUrl={mealItem.item.imageUrl}
+            duration={mealItem.item.duration}
+            complexity={mealItem.item.complexity.toUpperCase()}
+            affordability={mealItem.item.affordability.toUpperCase()}
+        />);
+    }
+
     return(
         <View style={styles.screen}>
-            <Text>Category Meals Screen!</Text>
-            <Text>{selectedId.title}</Text>
-            <Button title='Meal Details' onPress ={() => {
-                props.navigation.navigate('MealDetails');
-            }} />
-            <Button title='Go Back' onPress={()=>{
-                // we can also use goBack() instead of pop() 
-                // but goBack() is used for other navigation used as well
-                props.navigation.pop(); 
-            }}/>
+            <FlatList data={dispayedMeals} renderItem={renderMealItem} style={{width:'98%'}}/>
         </View>
     );
 }
